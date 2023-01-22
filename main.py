29fb9d063f8f7df81a5e6f2f4ff63d337e286cb0
Jedi-Coder1 <edmund.spielman@outlook.com>
2023-01-22 12:45:30 -0700

    i did funny

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ from time import sleep as S
 
 def main():
 	Title = Get.getWindowsWithTitle('Geometry Dash')
-	if Title == '[Win32Window(hWnd=29821340)]':
-		print(Title)
-	else:
+	if Title == []:
 		print('Geometry Dash is not opened')
 		S(3)
 		main()
+	else:
+		print(Title)
 
 
 main()
